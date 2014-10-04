21ad422d2de8908f3bdf31d17d22353ed3e7d3ac
Yogesh Sajanikar <yogesh_sajanikar@yahoo.com>
2014-10-04 12:11:50 +0530

    Added test template for fileblob

---
+++ NHibernate.FileBlob.NUnit/FileBlobMapTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace NHibernate.FileBlob.NUnit
+{
+    [TestFixture]
+    public class FileBlobMapTest
+    {
+        [Test]
+        public void Test()
+        {
+
+        }
+    }
+}
+
